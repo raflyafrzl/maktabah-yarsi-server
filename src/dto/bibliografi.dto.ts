5d0fd67594b8dd80d79d67e5ce86c7684fbff18b
raflyafrzl <raflyafrzlll@gmail.com>
2023-11-09 21:40:24 +0700

    feat: add schema doc for bibliografi

--- src/dto/bibliografi.dto.ts
+++ src/dto/bibliografi.dto.ts
@@ -1,13 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
 import * as Joi from 'joi';
 
 export class BibliografiCreateDTO {
+  @ApiProperty({
+    default: 'Kajian Fiqih',
+    description: 'title of bibliografi',
+    type: String,
+  })
   title: string;
+  @ApiProperty({
+    default: 'buku ini berisikan bla bla blaa',
+    type: String,
+    description: 'description for bibliografi',
+  })
   description: string;
+  @ApiProperty({
+    default: 'MUI',
+    type: String,
+    description: 'contributor the bibliografi',
+  })
   contributor: string;
+  @ApiProperty({
+    default: 'Ujang suracap',
+    type: String,
+    description: 'creator of the bibliografi',
+  })
   creator: string;
+  @ApiProperty({
+    default: 'Sinar mas',
+    type: String,
+    description: 'publisher of the bibliografi',
+  })
   publisher: string;
+  @ApiProperty({
+    default: 'https://google.com',
+    type: String,
+    description: 'where the bibliografi get from',
+  })
   source: string;
+  @ApiProperty({
+    type: String,
+    description: 'image of the bibliografi',
+    default:
+      'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+  })
   image_url: string;
+  @ApiProperty({
+    default: 'jqwe21kkiebxn21po',
+    type: String,
+    description: 'category of the bibliografi',
+  })
   category: string;
 }
 
