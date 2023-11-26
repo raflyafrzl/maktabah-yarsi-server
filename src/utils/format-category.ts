f6fb41b143f0160cf9fd3f797c7bf7fc8e77a765
raflyafrzl <raflyafrzlll@gmail.com>
2023-11-26 21:46:48 +0700

    refactor category format

--- src/utils/format-category.ts
+++ src/utils/format-category.ts
@@ -1,16 +1,16 @@
 import { Category } from 'src/schemas/category.schema';
 
-type CategoryNode = {
+type SubCategories = {
   id: string;
   name: string;
   total: number;
-  subcategories?: CategoryNode[] | null;
+  subcategories?: SubCategories[] | null;
 };
 
-export const buildCategoryTree = (categories: Category[]): CategoryNode[] => {
-  const categoryMap = new Map<string, any>();
+export const buildCategoryTree = (categories: Category[]): SubCategories[] => {
+  const mapCat = new Map<string, any>();
   categories.forEach((category) => {
-    categoryMap.set(category['_id'].toString(), {
+    mapCat.set(category['_id'].toString(), {
       id: category['_id'].toString(),
       name: category.name,
       total: category.total,
@@ -21,15 +21,17 @@ export const buildCategoryTree = (categories: Category[]): CategoryNode[] => {
   for (const category of categories) {
     const parentId = category.category ? category.category.toString() : null;
     if (parentId) {
-      const parentNode: CategoryNode = categoryMap.get(parentId);
-      if (parentNode) {
-        const childNode = categoryMap.get(category['_id'].toString());
-        if (childNode) {
-          parentNode.subcategories.push(childNode);
+      const parentCategorySub: SubCategories = mapCat.get(parentId);
+      if (parentCategorySub) {
+        const ChildCategorySub: SubCategories = mapCat.get(
+          category['_id'].toString(),
+        );
+        if (ChildCategorySub) {
+          parentCategorySub.subcategories.push(ChildCategorySub);
         }
       }
     }
   }
 
-  return Array.from(categoryMap.values());
+  return Array.from(mapCat.values());
 };
