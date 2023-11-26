8f4e0eccd103114c8dde38d0e63c56b3ef123993
raflyafrzl <raflyafrzlll@gmail.com>
2023-11-26 18:52:15 +0700

    fix recursive category

--- src/utils/format-category.ts
+++ src/utils/format-category.ts
@@ -1,30 +1,35 @@
 import { Category } from 'src/schemas/category.schema';
 
-type CategoryTree = Category & { _id: string };
-type ChildCategories = Omit<CategoryTree, 'category'> & {
-  subcategory?: ChildCategories;
+type CategoryNode = {
+  id: string;
+  name: string;
+  total: number;
+  subcategories?: CategoryNode[] | null;
 };
 
-export const buildCategoryTree = (categories: Category[], parentId = null) => {
-  const tree = [];
+export const buildCategoryTree = (categories: Category[]): CategoryNode[] => {
+  const categoryMap = new Map<string, any>();
+  categories.forEach((category) => {
+    categoryMap.set(category['_id'].toString(), {
+      id: category['_id'].toString(),
+      name: category.name,
+      total: category.total,
+      subcategories: [],
+    });
+  });
 
-  for (const category of categories as unknown as ReadonlyArray<CategoryTree>) {
-    const categoryId = category.category ? category.category.toString() : null;
-    if (categoryId === parentId) {
-      const subtree: ChildCategories = {
-        _id: category._id.toString(),
-        name: category.name,
-        total: category.total,
-      };
-
-      const children = buildCategoryTree(categories, category._id.toString());
-      if (children.length > 0) {
-        subtree.subcategory = children as unknown as ChildCategories;
+  for (const category of categories) {
+    const parentId = category.category ? category.category.toString() : null;
+    if (parentId) {
+      const parentNode: CategoryNode = categoryMap.get(parentId);
+      if (parentNode) {
+        const childNode = categoryMap.get(category['_id'].toString());
+        if (childNode) {
+          parentNode.subcategories.push(childNode);
+        }
       }
-
-      tree.push(subtree);
     }
   }
 
-  return tree;
+  return Array.from(categoryMap.values());
 };
